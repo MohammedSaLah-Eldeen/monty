d403b8fd7b8c2818d5fc6e5054c76646b96b5973
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-09-20 20:53:52 +0000

    Task 0

--- free_stack.c
+++ free_stack.c
@@ -9,12 +9,12 @@
 */
 void free_stack(stack_t *stack)
 {
-  stack_t *next;
+stack_t *next;
 
-  while (stack != NULL)
-    {
-      next = stack->next;
-      free(stack);
-      stack = next;
-    }
+while (stack != NULL)
+{
+next = stack->next;
+free(stack);
+stack = next;
+}
 }
--- pushf.c
+++ pushf.c
@@ -1,45 +1,45 @@
 #include "monty.h"
 
 /**
- * pushf - adds a layer to stack.
- * 
- * @stack: double pointer to the top of the stack.
- * @line_number: the line number.
- *
- * Return: nothing
- */
+* pushf - adds a layer to stack.
+* 
+* @stack: double pointer to the top of the stack.
+* @line_number: the line number.
+*
+* Return: nothing
+*/
 void pushf(stack_t **stack, unsigned int line_number)
 {
-  stack_t *new;
-  int i = 0;
-  int number;
-  
-  if (item == NULL)
-    {
-      fprintf(stderr, "L%u: usage: push integer", line_number);
-      exit(EXIT_FAILURE);
-    }
-  while (item[i] != '\0')
-    {
-    if (!isdigit(item[i]))
-      {
-	fprintf(stderr, "L%u: usage: push integer", line_number);
-	exit(EXIT_FAILURE);
-      }
-    i++;
-    }
-  number = atoi(item);
-  new = malloc(sizeof(stack_t));
-  if (new == NULL)
-    {
-      fprintf(stderr, "Error: malloc failed\n");
-      exit(EXIT_FAILURE);
-    }
-  new->n = number;
-  new->prev = NULL;
-  new->next = *stack;
-  *stack = new;
+stack_t *new;
+int i = 0;
+int number;
 
-  if (new->next != NULL)
-    (new->next)->prev = new;
+if (item == NULL)
+{
+fprintf(stderr, "L%u: usage: push integer", line_number);
+exit(EXIT_FAILURE);
+}
+while (item[i] != '\0')
+{
+if (!isdigit(item[i]))
+{
+fprintf(stderr, "L%u: usage: push integer", line_number);
+exit(EXIT_FAILURE);
+}
+i++;
+}
+number = atoi(item);
+new = malloc(sizeof(stack_t));
+if (new == NULL)
+{
+fprintf(stderr, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+new->n = number;
+new->prev = NULL;
+new->next = *stack;
+*stack = new;
+
+if (new->next != NULL)
+(new->next)->prev = new;
 }
